a7148f86b3bdc0fcd2c208f3a5437c671969fc10
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-19 19:45:36 +0600

    factoririal in function

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ for (var i = 1; i <= 10; i++) {
     factorial = factorial *i;
     console.log(i, factorial);
 }
+
+// function factorial(n) {
+//     var factorial = 1;
+//     for (var i = 1; i <= 10; i++) {
+//         factorial = factorial * i;
+//     }
+//     return (factorial);
+// } 
+// var result = factorial(10);
+// console.log(result);
